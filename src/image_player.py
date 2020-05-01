ee8692af12617e85949ba680d1611ebfae5c15f4
Sam at Laptop <Sam>
2020-05-01 11:20:49 -0400

    Update image_player.py

--- src/image_player.py
+++ src/image_player.py
@@ -20,7 +20,7 @@ class image_converter:
 
         #self.camera = cv2.VideoCapture(0)
         #self.image_list = glob.glob("/home/sam/Downloads/FLIR_ADAS_1_3/train/Annotated_thermal_8_bit/*.jpeg")
-        self.image_list = glob.glob("/root/Annotated_thermal_8_bit/*.jpeg")
+        self.image_list = glob.glob("/home/sam/Annotated_thermal_8_bit/*.jpeg")
         self.rate = rospy.Rate(10)
     
 
